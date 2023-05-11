ed87ae8dae5023917d0189b98fe49a478f882ded
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2023-05-11 20:20:02 +0530

    new classes and api added -> aastha

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //657704d15e584d94bc884e49d727bf5b
+
 
 
     @Override
